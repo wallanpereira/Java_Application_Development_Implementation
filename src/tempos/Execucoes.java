5e895893b6bb5bb169373c8a975fa1ee9e6be7a2
Wallan Pereira <32087983+wallanpereira@users.noreply.github.com>
2020-02-02 22:48:38 -0300

    Continuar no exercicio 5 cap 03

--- src/tempos/Execucoes.java
+++
@@ -1,9 +0,0 @@
-package tempos;
-
-public class Execucoes {
-	public void tabuada(int valor) {
-		for (int i = 1; i < 11; i++) {
-			System.out.println(i + " * " + valor + " = " + (i * valor));
-		}
-	}
-}
