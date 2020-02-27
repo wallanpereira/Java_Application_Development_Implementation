ac812cf1d7c98b258c2a6c8a37eaa95a2cd765b3
Wallan Pereira <32087983+wallanpereira@users.noreply.github.com>
2020-02-27 02:04:26 -0300

    atividade 4 cap 4

---
+++ src/atividade_cap_04/numeroSorteadoSenha.java
@@ -0,0 +1,22 @@
+package atividade_cap_04;
+
+import javax.swing.*;
+
+public class numeroSorteadoSenha {
+    public static void main(String[] args) {
+
+        /* Uma pequena correção: 5+(int)( 6*Math.random() ) -
+        * O cast deve ser feito depois da multiplicação, senão retorna sempre zero - multiplique por 6 pq o método retorna um numero de 0 a menor que 1, nunca retorna 1, então jamais daria 10.
+        */
+        int numeroSorteado = (int) (Math.random() * 6) + 5;
+        String senha = "";
+
+        for (int i = 0; i < numeroSorteado; i++) {
+            int numero = (int) (Math.random() * 9);
+            senha += numero;
+        }
+        JOptionPane.showMessageDialog(null,"Numero sorteado: " + numeroSorteado
+        + "\nSenha: " + senha);
+        System.exit(0);
+    }
+}
