c038afe0041851f9ad444dd96cdb9c8c1b834c59
Wallan Pereira <32087983+wallanpereira@users.noreply.github.com>
2020-02-27 01:33:19 -0300

    Atividade 3 cap 4

--- src/atividade_cap_04/AjustePercentualAcrescimo.java
+++ src/atividade_cap_04/AjustePercentualAcrescimo.java
@@ -1,13 +1,20 @@
-package atividade_cap_04;
+	package atividade_cap_04;
 
-public class AjustePercentualAcrescimo {
+	import javax.swing.*;
 
-	public static void main(String[] args) {
-		/*
-		 * Ajuste os preco em 12%
-		 * 
-		 */
+	public class AjustePercentualAcrescimo {
+
+		public static void main(String[] args) {
+			double valorInformado = 0;
+
+			valorInformado =  Double.parseDouble(JOptionPane.showInputDialog("Digite um valor: "));
+
+			double valorComAcrescimo = valorInformado + (valorInformado * 0.12);
+
+			JOptionPane.showMessageDialog(null,"Valor com acrescimo: " + valorComAcrescimo
+				+ "\nValo arredondado: " + Math.round(valorComAcrescimo));
+
+			System.exit(0);
+		}
 
 	}
-
-}
